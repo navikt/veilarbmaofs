2dda2ca00983dc2e0d29b3b031147420e751f52c
Oscar Eckbo <oscar.eckbo@nav.no>
2018-06-18 08:55:24 +0200

    [KAIZEN-0] legger til søkefelt og bruker fnr fra context

--- src/app/basisinfo/basisinfo.tsx
+++ src/app/basisinfo/basisinfo.tsx
@@ -1,24 +1,55 @@
 import * as React from 'react';
 
+import { getData, SourceConfig } from "../../fetch-utils";
+import { IAppProps } from "../application";
+import { IPersonaliaInfo } from "../visningskomponenter/personalia/personalia";
 import ApneLukkeKnapp from './apne-lukke-knapp';
 import KvinneIkon from './kvinne.svg';
 import MannIkon from './mann.svg';
 
+import Datafetcher from "../utils/datafetcher";
+import { kalkulerAlder } from "../utils/date-utils";
+
 import './basisinfo.less';
 
-function Basisinfo() {
-    const ikon = Math.random() > 0.5 ? KvinneIkon : MannIkon;
+function Basisinfo({ fnr }: IAppProps) {
+    const sourceConfig: SourceConfig<{personalia: string}> = {
+        personalia: `/veilarbperson/api/person/${fnr}`
+    };
+
+    const data = getData<{ personalia: IPersonaliaInfo }>(sourceConfig);
+
+    function FetchedData() {
+        return (
+            <Datafetcher data={data}>
+                { (el: { personalia: IPersonaliaInfo }) => <LagBasisInfo person={el.personalia}/> }
+            </Datafetcher>
+        );
+    }
+
+    function LagBasisInfo(props: { person: IPersonaliaInfo }) {
+        const alder = kalkulerAlder(new Date(props.person.fodselsdato));
+        const ikon = props.person.kjonn === "K" ? KvinneIkon : MannIkon;
+
+        return (
+            <>
+                <img src={ikon} className="basisinfo__ikon"/>
+                <div className="basisinfo__personalia">
+                    <div className="basisinfo__personalia">
+                        <h1 className="basisinfo__navnogalder typo-innholdstittel">
+                            {props.person.sammensattNavn}
+                            <span> {`${alder} år`}</span>
+                        </h1>
+                        <span className="basisinfo__fodselsnummer">{fnr}</span>
+                    </div>
+                </div>
+            </>
+        );
+    }
 
     return (
         <>
-            <img src={ikon} className="basisinfo__ikon"/>
-            <div className="basisinfo__personalia">
-                <h1 className="basisinfo__navnogalder typo-innholdstittel">
-                    {`Bruce Wayne`}
-                    <span> {`${`38`} år`}</span>
-                </h1>
-                <span className="basisinfo__fodselsnummer">{`1010800398`}</span>
-            </div>
+            <FetchedData />
             <div className="basisinfo__apnelukke">
                 <ApneLukkeKnapp />
             </div>
--- src/app/persondetaljer.tsx
+++ src/app/persondetaljer.tsx
@@ -1,19 +1,24 @@
 import * as cls from 'classnames';
 import * as React from 'react';
 
-import {IFetchContext} from "../config";
+import { IFetchContext } from "../config";
 import { AppContext, IAppContextProp, withAppContext } from "./context";
 
+import { IAppProps } from "./application";
 import Basisinfo from "./basisinfo/basisinfo";
 import Tilbakelenke from "./tilbakelenke";
 import Informasjonsvisning from "./visningskomponenter/informasjonsvisning";
 
 import './persondetaljer.less';
 
-class Persondetaljer extends React.Component<IAppContextProp> {
+class Persondetaljer extends React.Component<IAppContextProp & IAppProps> {
+    public shouldComponentUpdate(nextProps: any) {
+        return this.props.fnr !== nextProps.fnr;
+    }
+
     public render() {
         const apen = this.props.context.apen;
-        const fetchContext: IFetchContext = {fnr : "1234567899"};
+        const fetchContext: IFetchContext = { fnr : this.props.fnr };
 
         return (
             <>
@@ -24,7 +29,7 @@ class Persondetaljer extends React.Component<IAppContextProp> {
                         'lukket': !apen
                     })}
                 >
-                    <Basisinfo />
+                    <Basisinfo fnr={this.props.fnr} />
                     <Informasjonsvisning fetchContext={fetchContext} />
                 </div>
             </>
@@ -32,4 +37,4 @@ class Persondetaljer extends React.Component<IAppContextProp> {
     }
 }
 
-export default withAppContext<{}>(AppContext, Persondetaljer);
+export default withAppContext<IAppProps>(AppContext, Persondetaljer);
