8f4ec57229d54e770db0057fbff1a31b42c638e8
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2018-06-20 09:43:48 +0200

    [KAIZEN-0] Mer animasjoner

--- src/app/basisinfo/basisinfo.tsx
+++ src/app/basisinfo/basisinfo.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import ContentLoader from 'react-content-loader';
 import { getData, SourceConfig } from "../../fetch-utils";
 import { IAppProps } from "../application";
 import Datafetcher from "../utils/datafetcher";
 import { kalkulerAlder } from "../utils/date-utils";
 import { IPersonaliaInfo } from "../visningskomponenter/personalia/personalia";
+import ContentLoader from './../utils/content-loader';
 import ApneLukkeKnapp from './apne-lukke-knapp';
 import './basisinfo.less';
 import KvinneIkon from './kvinne.svg';
@@ -38,7 +38,6 @@ function Placeholder() {
         <ContentLoader height={94} width={1120} primaryColor="#C6C2BF" secondaryColor="#E9E7E7">
             <rect x="0" y="0" rx="5" ry="5" width="32" height="26" />
             <rect x="48" y="0" rx="5" ry="5" width="934.375" height="30" />
-            <rect x="339.3125" y="-2" rx="5" ry="5" width="74.609375" height="34" />
             <rect x="48" y="37" rx="5" ry="5" width="87.46875" height="14" />
             <rect x="1005.375" y="2" rx="5" ry="5" width="55.609375" height="14" />
         </ContentLoader>
