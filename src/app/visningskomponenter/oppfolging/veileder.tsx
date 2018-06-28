55221f98a4ffecf0a0832b3a4e10af3782b93d29
alexander-svendsen <alexander.svendsen@nav.no>
2018-06-28 16:52:12 +0200

    FO-1040: Ikke kall API når vi mangler veilederId

--- src/app/visningskomponenter/oppfolging/veileder.tsx
+++ src/app/visningskomponenter/oppfolging/veileder.tsx
@@ -6,13 +6,18 @@ import InformasjonsbolkEnkel from "../felles-komponenter/informasjonsbolk-enkel"
 import {StringOrNull} from "../felles-typer";
 import VeilederPlaceholder from "./veileder-placeholder";
 
+function veilederInformasjonbolkEnkel(value: StringOrNull, ...rest: any[]){
+    return <InformasjonsbolkEnkel header="Veileder"
+                                  value={value}
+                                  defaultValue="-"
+                                  {...rest}/>
+}
+
+
 export function renderVeileder(props: VeilederData) {
     const {ident, navn, ...rest} = props;
     const veilederStr = ident ? `${navn} (${ident})` : null;
-    return <InformasjonsbolkEnkel header="Veileder:" 
-                                  value={veilederStr} 
-                                  defaultValue="-"
-                                  {...rest}/>
+    return veilederInformasjonbolkEnkel(veilederStr, rest)
 }
 
 interface VeilederProps{
@@ -21,6 +26,11 @@ interface VeilederProps{
 
 export function Veileder(props: VeilederProps){
     const {veilederId, ...rest } = props;
+
+    if (!veilederId){
+        return veilederInformasjonbolkEnkel(null, rest);
+    }
+
     const sourceConfig: SourceConfig<{veileder: VeilederData}> = {
         veileder: `/veilarbveileder/api/veileder/${veilederId}`
     };
