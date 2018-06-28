1c482f33c7b307172b7dbcc435bf24c2ef381cc3
alexander-svendsen <alexander.svendsen@nav.no>
2018-06-28 14:06:48 +0200

    FO-1040: Vises feil i griden på IE

--- src/app/visningskomponenter/oppfolging/veileder.tsx
+++ src/app/visningskomponenter/oppfolging/veileder.tsx
@@ -6,13 +6,13 @@ import InformasjonsbolkEnkel from "../felles-komponenter/informasjonsbolk-enkel"
 import {StringOrNull} from "../felles-typer";
 import VeilederPlaceholder from "./veileder-placeholder";
 
-
-export function renderVeilder({ veileder }: { veileder: VeilederData }) {
+export function renderVeilder(data: { veileder: VeilederData }, rest: any) {
+    const veileder = data.veileder;
     const veilederStr = veileder ? `${veileder.navn} (${veileder.ident})` : null;
     return <InformasjonsbolkEnkel header="Veileder:" 
                                   value={veilederStr} 
-                                  defaultValue="-"/>
-
+                                  defaultValue="-"
+                                  {...rest}/>
 }
 
 interface VeilederProps{
@@ -20,15 +20,15 @@ interface VeilederProps{
 }
 
 export function Veileder(props: VeilederProps){
-
+    const {veilederId, ...rest } = props;
     const sourceConfig: SourceConfig<{veileder: VeilederData}> = {
-        veileder: `/veilarbveileder/api/veileder/${props.veilederId}`
+        veileder: `/veilarbveileder/api/veileder/${veilederId}`
     };
 
     const data = getData<{ veileder: VeilederData }>(sourceConfig);
 
     return <Datafetcher data={data} loader={VeilederPlaceholder}>
-        { renderVeilder }
+        { (fetchedData : { veileder: VeilederData }) => renderVeilder(fetchedData, rest) }
     </Datafetcher>
 
 }
