4f0d1bd97ddf9d8e93f9bccf44f2fcd7697c5e8b
hakonph <hakon.planke.holm@nav.no>
2018-08-30 14:54:02 +0200

    [FO-1447] fikser space mellom harder og inhold i profilering

--- src/app/visningskomponenter/registrering/profilering.tsx
+++ src/app/visningskomponenter/registrering/profilering.tsx
@@ -1,28 +1,27 @@
-    import { Ingress, Normaltekst} from "nav-frontend-typografi";
-    import * as React from "react";
-    import {Profilering} from "../../datatyper/registreringsData";
-    import FloatGrid from "../../utils/float-grid";
-    import Informasjonsbolk from "../felles-komponenter/informasjonsbolk";
+import {Element, Ingress, Normaltekst} from "nav-frontend-typografi";
+import * as React from "react";
+import {Profilering} from "../../datatyper/registreringsData";
+import FloatGrid from "../../utils/float-grid";
 
-    interface Props {
-        profilering?: Profilering;
+interface Props {
+    profilering?: Profilering;
+}
+
+export function Profilering(props: Props) {
+    if (!props.profilering) {
+        return null;
     }
 
-    export function Profilering(props: Props) {
-        if (!props.profilering) {
-            return null;
-        }
+    return (
+        <>
+            <Ingress>Hentet fra Aa-registret</Ingress>
+            <FloatGrid columns={2} gap={8}>
+                <div>
+                    <Element> Brukeren har vært sammenhengende i jobb minst 6 av de siste 12 måneder </Element>
+                    <Normaltekst>{props.profilering.jobbetSammenhengendeSeksAvTolvSisteManeder ? "Ja" : "Nei"} </Normaltekst>
+                </div>
+            </FloatGrid>
+        </>
 
-        const header = "Brukeren har vært sammenhengende i jobb minst 6 av de siste 12 måneder";
-        return (
-            <>
-                <Ingress>Hentet fra Aa-registret</Ingress>
-                <FloatGrid columns={2} gap={8}>
-                        <Informasjonsbolk header={header} headerTypo="element">
-                            <Normaltekst className="underinformasjon">{props.profilering.jobbetSammenhengendeSeksAvTolvSisteManeder ? "Ja" : "Nei"} </Normaltekst>
-                        </Informasjonsbolk>
-                </FloatGrid>
-            </>
-
-        )
-    }
+    )
+}
