b3fc7c997d82f2bb2d949bfdecbb458e19989b43
hakonph <hakon.planke.holm@nav.no>
2018-08-30 10:51:54 +0200

    [FO-1447] feature togling av registrering og endring av tekster

--- src/app/visningskomponenter/registrering/profilering.tsx
+++ src/app/visningskomponenter/registrering/profilering.tsx
@@ -1,21 +1,24 @@
 import {Element, Ingress, Normaltekst} from "nav-frontend-typografi";
 import * as React from "react";
 import {Profilering} from "../../datatyper/registreringsData";
+import FloatGrid from "../../utils/float-grid";
 
 interface Props {
     profilering?: Profilering;
 }
 
 export function Profilering(props: Props) {
-    if(!props.profilering) {
+    if (!props.profilering) {
         return null;
     }
 
-    return(
+    return (
         <>
             <Ingress>Hentet fra Aa-registret</Ingress>
-            <Element> Brukeren har vært sammenhengende i jobb minst 6 av de siste 12 måneder </Element>
-            <Normaltekst>{props.profilering.jobbetSammenhengendeSeksAvTolvSisteManeder ? "Ja" : "Nei"} </Normaltekst>
+            <FloatGrid columns={2} gap={8}>
+                <Element> Brukeren har vært sammenhengende i jobb minst 6 av de siste 12 måneder </Element>
+                <Normaltekst>{props.profilering.jobbetSammenhengendeSeksAvTolvSisteManeder ? "Ja" : "Nei"} </Normaltekst>
+            </FloatGrid>
         </>
 
     )
