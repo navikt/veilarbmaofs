c782056631491fc8edae4f9f1426d5793fb7bc8a
Oscar Eckbo <oscareckbo@users.noreply.github.com>
2018-08-30 14:59:47 +0200

    [FO-1447]  skriveleif

--- src/app/visningskomponenter/registrering/profilering.tsx
+++ src/app/visningskomponenter/registrering/profilering.tsx
@@ -14,7 +14,7 @@ export function Profilering(props: Props) {
 
     return (
         <>
-            <Ingress>Hentet fra Aa-registret</Ingress>
+            <Ingress>Hentet fra Aa-registeret</Ingress>
             <FloatGrid columns={2} gap={8}>
                 <div>
                     <Element> Brukeren har vært sammenhengende i jobb minst 6 av de siste 12 måneder </Element>
