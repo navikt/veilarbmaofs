7c26efc3d7f3a72cfe8a1062ff748bcceaea9723
alexander-svendsen <alexander.svendsen@nav.no>
2018-09-11 12:44:06 +0200

    FO-1482: Track opening of base components
    Also track the specific opening/closing of the different pannels

--- src/app/visningskomponenter/informasjonsvisning.tsx
+++ src/app/visningskomponenter/informasjonsvisning.tsx
@@ -1,31 +1,13 @@
-import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 import * as React from 'react';
 import {FetchContext, getConfig, IInformasjonsElement} from "../../config";
 import {AppContext, AppContextProp, IAppContext, withAppContext} from "../context";
-import Datafetcher from "../utils/datafetcher";
-
 import {Feature} from "../persondetaljer";
 import './informasjonsvisning.less';
-
-function onClick(id: string) {
-    return () => (window as any).frontendlogger.event('maofs.lamel-click', {'lamel': id}, {});
-}
-
-function VisningsBolk<DATA>(props: IInformasjonsElement<DATA> & AppContextProp) {
-    const Component = props.component;
-
-    return (
-        <Ekspanderbartpanel tittel={props.id} onClick={onClick(props.id)} tittelProps="undertittel">
-            <Datafetcher data={props.dataSource}>
-                {(data: DATA) => <Component data={data}/>}
-            </Datafetcher>
-        </Ekspanderbartpanel>
-    );
-}
+import {VisningsBolk} from "./visningsbolk";
 
 function lagVisningBolk<T>(context: IAppContext) {
     return (element: IInformasjonsElement<T>, index: number) => (
-        <VisningsBolk {...element} key={index} context={context} />
+        <VisningsBolk {...element} key={index} context={context}/>
     );
 }
 
@@ -35,7 +17,12 @@ interface Props {
 }
 
 class Informasjonsvisning extends React.Component<AppContextProp & Props> {
+    public componentDidMount() {
+        (window as any).frontendlogger.event('maofs.detaljer', {}, {});
+    }
+
     public render() {
+
         const renderElementer: React.ReactNode[] = getConfig(this.props.fetchContext, this.props.feature)
             .map(lagVisningBolk(this.props.context));
 
