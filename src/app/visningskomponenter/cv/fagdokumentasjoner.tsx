8d26d521c850c4d6d991157e16f607a96ed8fef0
Alexander Gård <alexander.gard@nav.no>
2019-03-28 09:47:48 +0100

    FO-2085 La til visning for fagdokumentasjoner

---
+++ src/app/visningskomponenter/cv/fagdokumentasjoner.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { ArenaPerson, FagdokumentType } from '../../datatyper/arenaperson';
+import Informasjonsbolk from '../felles-komponenter/informasjonsbolk';
+import { Element, Normaltekst } from 'nav-frontend-typografi'
+
+type Props = Pick<ArenaPerson, 'fagdokumentasjoner'>;
+
+const fagdokumentTypeTilTekst = (fagdokumentType: FagdokumentType): string => {
+    switch (fagdokumentType) {
+        case FagdokumentType.AUTORISASJON:
+            return 'Autorisasjon';
+        case FagdokumentType.MESTERBREV:
+            return 'Mesterbrev';
+        case FagdokumentType.SVENNEBREV_FAGBREV:
+            return 'Fagbrev/Svennebrev';
+        default:
+            return '';
+    }
+};
+
+function Fagdokumentasjoner(props: Props) {
+
+    if (!props.fagdokumentasjoner) {
+        return null;
+    }
+
+    const dokumentasjoner = props.fagdokumentasjoner.map((fagdokument, idx) => {
+        return (
+            <div key={`fagdokument-${idx}`} className="underinformasjon">
+                <Element>
+                    {fagdokumentTypeTilTekst(fagdokument.type)}
+                </Element>
+                <Normaltekst>{fagdokument.tittel}</Normaltekst>
+            </div>
+        );
+    });
+
+    return (
+        <Informasjonsbolk header="Fagdokumentasjoner" headerTypo="ingress">
+            {dokumentasjoner}
+        </Informasjonsbolk>
+    );
+}
+
+export default Fagdokumentasjoner;
