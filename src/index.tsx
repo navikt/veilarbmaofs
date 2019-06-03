6d3cdff6d7b0f516e460d68c9c06bf3921233655
alexandraback <alexandra1.back@gmail.com>
2019-06-03 15:32:25 +0200

    KAIZEN-0: Fjern form og ting

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import Application from './app/application';
 import NAVSPA from './NAVSPA';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // tslint:disable
 if (!(global as any)._babelPolyfill) {
@@ -8,7 +10,7 @@ if (!(global as any)._babelPolyfill) {
 
 if (process.env.REACT_APP_MOCK === 'true') {
     require('./mock');
+    ReactDOM.render( <Application fnr="10108000398" enhet="1234"/>, document.getElementsByClassName('main')[0])
+} else {
+    NAVSPA.eksporter('veilarbmaofs', Application);
 }
-// tslint:enable
-
-NAVSPA.eksporter('veilarbmaofs', Application);
