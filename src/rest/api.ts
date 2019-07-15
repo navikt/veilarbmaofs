b0c006c3fdcf92eec567e7d48018e342753c6be4
Alexander Gård <alexander.gard@nav.no>
2019-07-15 16:59:51 +0200

    FO-2367 La til oppretting av fetch info for alle API kall

--- src/rest/api.ts
+++ src/rest/api.ts
@@ -4,10 +4,42 @@ export interface FnrFetchParams {
     fnr: string;
 }
 
+export interface HentVeilederFetchParams {
+    veilderId: string;
+}
+
 export const lagHentPersonaliaFetchInfo = (params: FnrFetchParams): FetchInfo => ({
-    url: '',
+    url: `/veilarbperson/api/person/${params.fnr}`,
 });
 
 export const lagHentRegistreringFetchInfo = (params: FnrFetchParams): FetchInfo => ({
     url: `/veilarbregistrering/api/registrering?fnr=${params.fnr}`,
+});
+
+export const lagHentCvFetchInfo = (params: FnrFetchParams): FetchInfo => ({
+    url: `/pam-cv-api/rest/v1/arbeidssoker/${params.fnr}`,
+});
+
+export const lagHentVeilederFetchInfo = (params: HentVeilederFetchParams): FetchInfo => ({
+    url: `/veilarbveileder/api/veileder/${params.veilderId}`,
+});
+
+export const lagHentAktorIdFetchInfo = (params: FnrFetchParams): FetchInfo => ({
+    url: `/veilarbperson/api/person/aktorid?fnr=${params.fnr}`,
+});
+
+export const lagHentOppfolgingsstatusFetchInfo = (params: FnrFetchParams): FetchInfo => ({
+    url: `/veilarboppfolging/api/person/${params.fnr}/oppfolgingsstatus`,
+});
+
+export const lagHentJobbsokerkompetanseFetchInfo = (params: FnrFetchParams): FetchInfo => ({
+    url: `/veilarbjobbsokerkompetanse/api/hent?fnr=${params.fnr}`,
+});
+
+export const lagHentYtelserFetchInfo = (params: FnrFetchParams): FetchInfo => ({
+    url: `/veilarboppfolging/api/person/${params.fnr}/ytelser`,
+});
+
+export const lagHentUnderOppfolgingFetchInfo = (params: FnrFetchParams): FetchInfo => ({
+    url: `/veilarboppfolging/api/underoppfolging?fnr=${params.fnr}`,
 });
