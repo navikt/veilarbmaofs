1b525faf55be5d589bfa38c3a0c87a15c1e3a44b
AlexGaard <AlexGaard@users.noreply.github.com>
2020-01-16 12:16:40 +0000

    Automated deployment: Thu Jan 16 12:16:40 UTC 2020 533d8c3f6f0ab844fc6f924c9453cf15a0a2d103

---
+++ precache-manifest.fc77478edf354e2269fa70f9b072164e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da334ff5a771400e204abad9d9c34534",
+    "url": "/veilarbmaofs/index.html"
+  },
+  {
+    "revision": "acd607958a6a0e389167",
+    "url": "/veilarbmaofs/static/css/main.css"
+  },
+  {
+    "revision": "acd607958a6a0e389167",
+    "url": "/veilarbmaofs/static/js/main.js"
+  },
+  {
+    "revision": "85a00e2e9d2a4bfdd472b41769962502",
+    "url": "/veilarbmaofs/static/media/last-ned.85a00e2e.svg"
+  },
+  {
+    "revision": "f2de876d58aef7d9aa1caca576c03217",
+    "url": "/veilarbmaofs/static/media/printer.f2de876d.svg"
+  },
+  {
+    "revision": "aaff9ad12c06e553da2f4244d8162d84",
+    "url": "/veilarbmaofs/static/media/rediger-fylld.aaff9ad1.svg"
+  },
+  {
+    "revision": "a0d6689dd07169a0ed9681d30e54feaa",
+    "url": "/veilarbmaofs/static/media/rediger.a0d6689d.svg"
+  }
+]);
