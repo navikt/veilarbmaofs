cf53610ec4bfa68d27d320272cbf83e8ce90346c
Sneha Desai <sneha.desai@nav.no>
2021-06-08 15:07:27 +0200

    TK-96: Impoterte frontendlogger fra navikt

--- src/utils/frontend-logger.ts
+++ src/utils/frontend-logger.ts
@@ -1,9 +1,8 @@
-const frontendlogger = (window as any).frontendlogger;
+import { createFrontendLogger, DEFAULT_FRONTENDLOGGER_API_URL } from '@navikt/frontendlogger/lib';
+import { APP_NAME } from './konstanter';
+
+export const logger = createFrontendLogger(APP_NAME, DEFAULT_FRONTENDLOGGER_API_URL);
 
 export const logEvent = (logTag: string, fields?: {}, tags?: {}): void => {
-	if (frontendlogger && frontendlogger.event) {
-		frontendlogger.event(logTag, fields ? fields : {}, tags ? tags : {});
-	} else {
-		console.log('Event', logTag, 'Fields:', fields, 'Tags:', tags); // tslint:disable-line
-	}
+	logger.event(`${APP_NAME}.logevent.${logTag}`, fields, tags);
 };
