2054e8bcfff4266aebec2f537114ad5b1b2597f7
Alexander Gård <alex.gaard@hotmail.com>
2021-06-23 14:56:42 +0200

    La til Nav-Consumer-Id header på alle fetch-kall

--- src/rest/api.ts
+++ src/rest/api.ts
@@ -14,39 +14,44 @@ import { TOGGLES, Features } from './datatyper/feature';
 import { VergeOgFullmaktData} from "./datatyper/vergeOgFullmakt";
 import { TilrettelagtKommunikasjonData} from "./datatyper/tilrettelagtKommunikasjon";
 import { Innsatsbehov } from './datatyper/innsatsbehov';
+import { APP_NAME } from '../utils/konstanter';
+
+const headers = {
+	headers: { 'Nav-Consumer-Id': APP_NAME }
+};
 
 export const useFetchRegistrering = (fnr: string) =>
-	useFetch<RegistreringsData>(`/veilarbregistrering/api/registrering?fnr=${fnr}`);
+	useFetch<RegistreringsData>(`/veilarbregistrering/api/registrering?fnr=${fnr}`, headers);
 
 export const useFetchCvOgJobbprofil = (fnr: string) => useFetch<ArenaPerson>(`/veilarbperson/api/person/cv_jobbprofil?fnr=${fnr}`);
 
 export const useFetchVeileder = (veilederId: OrNothing<string>, config?: Config) =>
-	useFetch<VeilederData>(`/veilarbveileder/api/veileder/${veilederId}`, {}, config);
+	useFetch<VeilederData>(`/veilarbveileder/api/veileder/${veilederId}`, headers, config);
 
-export const useFetchAktorId = (fnr: string) => useFetch<AktorId>(`/veilarbperson/api/person/aktorid?fnr=${fnr}`);
+export const useFetchAktorId = (fnr: string) => useFetch<AktorId>(`/veilarbperson/api/person/aktorid?fnr=${fnr}`, headers);
 
 export const useFetchOppfolgingsstatus = (fnr: string) =>
-	useFetch<OppfolgingsstatusData>(`/veilarboppfolging/api/person/${fnr}/oppfolgingsstatus`);
+	useFetch<OppfolgingsstatusData>(`/veilarboppfolging/api/person/${fnr}/oppfolgingsstatus`, headers);
 
 export const useFetchJobbsokerkompetanse = (fnr: string) =>
-	useFetch<KartleggingData>(`/veilarbjobbsokerkompetanse/api/hent?fnr=${fnr}`);
+	useFetch<KartleggingData>(`/veilarbjobbsokerkompetanse/api/hent?fnr=${fnr}`, headers);
 
-export const useFetchYtelser = (fnr: string) => useFetch<YtelseData>(`/veilarboppfolging/api/person/${fnr}/ytelser`);
+export const useFetchYtelser = (fnr: string) => useFetch<YtelseData>(`/veilarboppfolging/api/person/${fnr}/ytelser`, headers);
 
 export const useFetchUnderOppfolging = (fnr: string) =>
-	useFetch<UnderOppfolgingData>(`/veilarboppfolging/api/underoppfolging?fnr=${fnr}`);
+	useFetch<UnderOppfolgingData>(`/veilarboppfolging/api/underoppfolging?fnr=${fnr}`, headers);
 
-export const useFetchPersonalia = (fnr: string) => useFetch<PersonaliaInfo>(`/veilarbperson/api/person/${fnr}`);
+export const useFetchPersonalia = (fnr: string) => useFetch<PersonaliaInfo>(`/veilarbperson/api/person/${fnr}`, headers);
 
-export const useFetchPersonaliaV2 = (fnr: string) => useFetch<PersonaliaV2Info>(`/veilarbperson/api/v2/person?fnr=${fnr}`);
+export const useFetchPersonaliaV2 = (fnr: string) => useFetch<PersonaliaV2Info>(`/veilarbperson/api/v2/person?fnr=${fnr}`, headers);
 
-export const useFetchVergOgFullmakt = (fnr: string) => useFetch<VergeOgFullmaktData>(`/veilarbperson/api/v2/person/vergeOgFullmakt?fnr=${fnr}`);
+export const useFetchVergOgFullmakt = (fnr: string) => useFetch<VergeOgFullmaktData>(`/veilarbperson/api/v2/person/vergeOgFullmakt?fnr=${fnr}`, headers);
 
-export const useFetchSpraakTolk = (fnr: string) => useFetch<TilrettelagtKommunikasjonData>(`/veilarbperson/api/v2/person/tolk?fnr=${fnr}`);
+export const useFetchSpraakTolk = (fnr: string) => useFetch<TilrettelagtKommunikasjonData>(`/veilarbperson/api/v2/person/tolk?fnr=${fnr}`, headers);
 
 export const useFetchInnsatsbehov = (fnr: string) =>
-	useFetch<Innsatsbehov>(`/veilarbvedtaksstotte/api/innsatsbehov?fnr=${fnr}`);
-
+	useFetch<Innsatsbehov>(`/veilarbvedtaksstotte/api/innsatsbehov?fnr=${fnr}`, headers);
 
 const toggles = TOGGLES.map(element => 'feature=' + element).join('&');
-export const useFetchFeatureToggle = () => useFetch<Features>(`/veilarbpersonflatefs/api/feature?${toggles}`);
+
+export const useFetchFeatureToggle = () => useFetch<Features>(`/veilarbpersonflatefs/api/feature?${toggles}`, headers);
