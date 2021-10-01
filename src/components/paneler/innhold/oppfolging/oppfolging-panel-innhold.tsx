c51b60ba932e7a3f653f151de0eb70a10bf0f51e
matiasvinjevoll <matias.vinjevoll@nav.no>
2021-10-01 13:59:25 +0200

    Viser enten servicegruppe eller innsatsgruppe+hovedmål Dersom servicegruppe fra Arena er en innsatsgruppe, så vises innsatsgruppe+hovedmål fra vedtaksstøtte. Hvis ikke, vises bare servicegruppe fra Arena.
    Co-authored-by: emildohlenhansen <emil.dolen.hansen@nav.no>

--- src/components/paneler/innhold/oppfolging/oppfolging-panel-innhold.tsx
+++ src/components/paneler/innhold/oppfolging/oppfolging-panel-innhold.tsx
@@ -20,6 +20,7 @@ import {
 	mapInnsatsgruppeTilTekst,
 	mapServicegruppeTilTekst
 } from '../../../../utils/text-mapper';
+import { erInnsatsgruppe } from '../../../../utils/arena-status-utils';
 
 const OppfolgingPanelInnhold = () => {
 	const { fnr } = useAppStore();
@@ -46,16 +47,31 @@ const OppfolgingPanelInnhold = () => {
 	const innsatsbehovData = hasData(innsatsbehov) ? innsatsbehov.data : null;
 	const oppfolgingsstatusData = hasData(oppfolgingsstatus) ? oppfolgingsstatus.data : null;
 
+
+	let servicegruppe = oppfolgingsstatusData?.servicegruppe;
+	let innsatsgruppe = innsatsbehovData?.innsatsgruppe;
+	let hovedmal = innsatsbehovData?.hovedmal;
+	// Vi bruker servicegruppe fra Arena som master for om vi skal vise servicegruppe eller innsatsgruppe + hovedmål:
+	// Hvis servicegruppe fra Arena er en innsatsgruppe, så viser vi innsatsgruppe + hovedmål fra vedtaksstøtte.
+	// Ellers viser vi bare servicegruppe fra Arena, siden det da har blitt satt en servicegruppe i Arena som er
+	// nyere enn innsagsgruppe + hovedmål fra vedtaksstøtte.
+	if (erInnsatsgruppe(oppfolgingsstatusData?.servicegruppe)) {
+		servicegruppe = undefined;
+	} else {
+		innsatsgruppe = undefined;
+		hovedmal = undefined;
+	}
+
 	return (
 		<Grid columns={4} gap="0.5rem">
 			<InformasjonsbolkEnkel
 				header="Servicegruppe"
-				value={mapServicegruppeTilTekst(oppfolgingsstatusData?.servicegruppe)}
+				value={mapServicegruppeTilTekst(servicegruppe)}
 				defaultValue={EMDASH}
 			/>
 			<InformasjonsbolkEnkel
 				header="Innsatsgruppe"
-				value={mapInnsatsgruppeTilTekst(innsatsbehovData?.innsatsgruppe)}
+				value={mapInnsatsgruppeTilTekst(innsatsgruppe)}
 				defaultValue={EMDASH}
 			/>
 			<InformasjonsbolkEnkel header="Veileder" value={hentVeilederTekst(veilederData)} defaultValue={EMDASH} />
@@ -71,7 +87,7 @@ const OppfolgingPanelInnhold = () => {
 			/>
 			<InformasjonsbolkEnkel
 				header="Hovedmål"
-				value={mapHovedmalTilTekst(innsatsbehovData?.hovedmal)}
+				value={mapHovedmalTilTekst(hovedmal)}
 				defaultValue={EMDASH}
 			/>
 		</Grid>
