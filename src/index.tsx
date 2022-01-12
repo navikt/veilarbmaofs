ed971ea03fe3e08209e5c11c62a6178661bd21a0
taniaholst <tania.holst@nav.no>
2022-01-12 13:52:45 +0100

    tc-100 modal

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@navikt/ds-css';
 import { Navspa, AsyncNavspa } from '@navikt/navspa';
-import ModalWrapper from 'nav-frontend-modal';
 import App from './app';
 import env from './utils/environment';
 import { fnr, enhet } from './mock/app';
 import { tilretteleggingsbehovSpaConfig } from './components/tilretteleggingsbehov-spa';
+import { Modal } from '@navikt/ds-react';
 
-ModalWrapper.setAppElement(document.getElementById('modal-a11y-wrapper'));
+Modal.setAppElement?.(document.getElementById('modal-a11y-wrapper'));
 
 if (env.isMocked) {
 	require('./mock');
