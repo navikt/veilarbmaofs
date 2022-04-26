b3650d1acc2289ae6871b0afb542396ded9096ae
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2022-04-26 09:43:55 +0200

    Tc-119 Bytt ut use-async og use-fetch, med løsning fra amt-tiltaksansvarligflate

--- src/rest/datatyper/feature.ts
+++ src/rest/datatyper/feature.ts
@@ -3,8 +3,6 @@ export const SPOR_OM_TILBAKEMELDING = 'veilarbmaofs.spor.om.tilbakemelding';
 export const INNSATSGRUPPE_OG_HOVEDMAL_FRA_VEDTAKSSTOTTE =
 	'veilarbmaofs.hent_innsatsgruppe_og_hovedmal_fra_vedtaksstotte';
 
-export const TOGGLES = [PERSONALIA_DATA_FRA_TPS, SPOR_OM_TILBAKEMELDING, INNSATSGRUPPE_OG_HOVEDMAL_FRA_VEDTAKSSTOTTE];
-
 export interface Features {
 	[PERSONALIA_DATA_FRA_TPS]: boolean;
 	[SPOR_OM_TILBAKEMELDING]: boolean;
