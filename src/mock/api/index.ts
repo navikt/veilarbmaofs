b25fa467391153230855d9901883b25a0ccbc17d
stianStensli <stian.horn.stensli@nav.no>
2022-07-07 14:52:25 +0200

    går vekk fra frontendlogger

--- src/mock/api/index.ts
+++ src/mock/api/index.ts
@@ -1,5 +1,4 @@
 import { RequestHandlersList } from 'msw/lib/types/setupWorker/glossary';
-import { frontendloggerHandlers } from './frontendlogger';
 import { veilarboppfolgingHandlers } from './veilarboppfolging';
 import { veilarbpersonHandlers } from './veilarbperson';
 import { veilarbpersonflatefsHandlers } from './veilarbpersonflatefs';
@@ -7,7 +6,6 @@ import { veilarbveilederHandlers } from './veilarbveileder';
 import { veilarbvedtaksstotteHandlers } from './veilarbvedtaksstotte';
 
 export const allHandlers: RequestHandlersList = [
-	...frontendloggerHandlers,
 	...veilarboppfolgingHandlers,
 	...veilarbpersonHandlers,
 	...veilarbpersonflatefsHandlers,
