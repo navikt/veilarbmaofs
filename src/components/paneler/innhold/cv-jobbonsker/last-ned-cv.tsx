591a8b55d17b0a23f87cb568db5415b28843737d
Mathias S. Müller <mathias.sundt.muller@nav.no>
2022-07-25 14:09:40 +0200

    Oppdater CV-lenker til nye DS

--- src/components/paneler/innhold/cv-jobbonsker/last-ned-cv.tsx
+++ src/components/paneler/innhold/cv-jobbonsker/last-ned-cv.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Lenke from 'nav-frontend-lenker';
 import { logger } from '../../../../utils/logger';
 import { Download, DownloadFilled } from '@navikt/ds-icons';
+import { Link } from '@navikt/ds-react';
 
 export function LastNedCV(props: { erManuell: boolean; lastNedCvLenke: string }) {
 	const [hover, setHover] = useState(false);
@@ -11,16 +12,16 @@ export function LastNedCV(props: { erManuell: boolean; lastNedCvLenke: string })
 	};
 
 	return (
-		<Lenke
+		<Link
 			onClick={handleOnLastNedLenkeClicked}
+			onMouseEnter={() => setHover(true)}
+			onMouseLeave={() => setHover(false)}
 			href={props.lastNedCvLenke}
 			className="cv-panel-lenke"
 			target="_blank"
 		>
-			<span onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-				{hover ? <DownloadFilled /> : <Download />}
-				Last ned CV
-			</span>
-		</Lenke>
+			{hover ? <DownloadFilled /> : <Download />}
+			Last ned CV
+		</Link>
 	);
 }
--- src/components/paneler/innhold/cv-jobbonsker/rediger-cv.tsx
+++ src/components/paneler/innhold/cv-jobbonsker/rediger-cv.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Lenke from 'nav-frontend-lenker';
 import { Edit, EditFilled } from '@navikt/ds-icons';
+import { Link } from '@navikt/ds-react';
 
 export function RedigerCV(props: { erManuell: boolean; cvRegistreringsLenke: string }) {
 	const [hover, setHover] = useState(false);
@@ -10,11 +11,15 @@ export function RedigerCV(props: { erManuell: boolean; cvRegistreringsLenke: str
 	}
 
 	return (
-		<Lenke href={props.cvRegistreringsLenke} className="cv-panel-lenke" target="_blank">
-			<span onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-				{hover ? <EditFilled /> : <Edit />}
-				Endre CV
-			</span>
-		</Lenke>
+		<Link
+			onMouseEnter={() => setHover(true)}
+			onMouseLeave={() => setHover(false)}
+			href={props.cvRegistreringsLenke}
+			className="cv-panel-lenke"
+			target="_blank"
+		>
+			{hover ? <EditFilled /> : <Edit />}
+			Endre CV
+		</Link>
 	);
 }
