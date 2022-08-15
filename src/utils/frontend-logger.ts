e1de625e76f0928ef022849ea4897dac8254351e
stianStensli <stian.horn.stensli@nav.no>
2022-08-15 10:21:51 +0200

    logger

--- src/utils/frontend-logger.ts
+++
@@ -1,8 +0,0 @@
-import { createFrontendLogger, DEFAULT_FRONTENDLOGGER_API_URL } from '@navikt/frontendlogger/lib';
-import { APP_NAME } from './konstanter';
-
-export const logger = createFrontendLogger(APP_NAME, DEFAULT_FRONTENDLOGGER_API_URL);
-
-export const logEvent = (logTag: string, fields?: {}, tags?: {}): void => {
-	logger.event(`${APP_NAME}.logevent.${logTag}`, fields, tags);
-};
