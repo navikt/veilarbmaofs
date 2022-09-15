dda02f8e91191350c32e37f09e5993741b0f9a5f
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2022-09-15 15:33:31 +0200

    Legge til nødvendige eslint ignore linjer

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -8,7 +8,7 @@ export interface FrontendEvent {
 
 export const logMetrikk = (metrikkNavn: string, fields?: {}, tags?: {}): void => {
 	if (process.env.REACT_APP_DEV === 'true') {
-		// tslint:disable-next-line:no-console
+		// eslint-disable-next-line no-console
 		console.log('Event', metrikkNavn, 'Fields:', fields, 'Tags:', tags);
 	} else {
 		sendEventTilVeilarbperson({ name: `${metrikkNavn}`, fields, tags });
