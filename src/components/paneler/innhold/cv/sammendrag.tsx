945471087f857c15ac90264c800b8f88a44a47b2
Mathias S. Müller <mathias.sundt.muller@nav.no>
2022-09-29 16:38:06 +0200

    Bytt ikon i sammendrag-komponent

--- src/components/paneler/innhold/cv/sammendrag.tsx
+++ src/components/paneler/innhold/cv/sammendrag.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { ArenaPerson } from '../../../../rest/datatyper/arenaperson';
 import Informasjonsbolk from '../../../felles/informasjonsbolk';
 import { visEmdashHvisNull } from '../../../../utils';
-import { Heart } from '@navikt/ds-icons';
+import { List } from '@navikt/ds-icons';
 import { BodyShort } from '@navikt/ds-react';
 
 function Sammendrag(props: Pick<ArenaPerson, 'sammendrag'>) {
 	return (
-		<Informasjonsbolk header="Sammendrag" headerTypo="ingress" icon={<Heart />}>
+		<Informasjonsbolk header="Sammendrag" headerTypo="ingress" icon={<List />}>
 			<BodyShort className="underinformasjon cv-sammendrag">{visEmdashHvisNull(props.sammendrag)}</BodyShort>
 		</Informasjonsbolk>
 	);
