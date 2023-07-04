3562c77c63edc083d40ebc8bdb3cc751227ecd4d
Dragutin Vujovic <dragutin.vujovic@nav.no>
2023-07-04 12:35:46 +0200

    Update dev url

--- src/utils/miljo-utils.ts
+++ src/utils/miljo-utils.ts
@@ -1,7 +1,7 @@
 export const NAIS_PREPROD_SUFFIX = 'nais.preprod.local';
 export const NAIS_PROD_SUFFIX = 'nais.adeo.no';
 
-export const NAV_INTERN_PREPROD_SUFFIX = 'intern.dev.nav.no';
+export const NAV_INTERN_PREPROD_SUFFIX = 'dev.intern.nav.no';
 export const NAV_INTERN_PROD_SUFFIX = 'intern.nav.no';
 
 const DEV_DOMAINS = ['dev', 'app-q1', 'app-q0', 'localhost'];
